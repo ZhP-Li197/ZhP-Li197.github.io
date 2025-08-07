2808e23233af9c633dabf68bd32d6bd458dc5bad
ZhP-Li197 <zhengpinli197@gmail.com>
2025-08-07 15:39:23 +0000

    Deploying to gh-pages from @ ZhP-Li197/ZhP-Li197.github.io@9610c1e73f9e72bc1379ed1863661448c92688b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/CV/";
